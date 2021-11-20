984d2d6e1a287ba2c0d0a1f9125314c5a66dfd29
Dhruv <190310patel@gmail.com>
2021-11-20 17:17:55 +0530

    DigitalClockAdded

--- analog.js
+++ analog.js
@@ -15,3 +15,20 @@ setInterval(() => {
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
 }, 1000);
+let a;
+let date;
+let time;
+const option = {
+    weekday: 'long',
+    years: 'numeric',
+    month: 'long',
+    day: 'numeric',
+};
+
+setInterval(() => {
+    a = new Date();
+    date = a.toLocaleDateString(undefined, option);
+    time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+    document.getElementById('time').innerHTML = time;
+    document.getElementById('day').innerHTML = date;
+}, 1000);
